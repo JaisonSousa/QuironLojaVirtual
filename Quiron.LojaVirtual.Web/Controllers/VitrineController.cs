c71d213e4f28505c0d07a661537b76016782cecd
JaisonSousa <jaison.sp13@gmail.com>
2016-05-16 23:02:22 -0300

    Desenvolvimento

---
+++ Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Quiron.LojaVirtual.Dominio.Repositorio;
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class VitrineController : Controller
+    {
+        private ProdutosRepositorio _repositorio;
+
+        //paginação
+        public int ProdutoPorPagina = 3;
+        //
+        // GET: /Vitrine/
+        public ActionResult ListaProdutos(int pagina = 1 )
+        {
+            _repositorio = new ProdutosRepositorio();
+            var produtos = _repositorio.Produtos.OrderBy(p => p.Descricao)
+                .Skip((pagina - 1) * ProdutoPorPagina)
+                .Take(ProdutoPorPagina);
+
+            return View(produtos);
+        }
+	}
+}
---
+++ Quiron.LojaVirtual.Web/Models/Paginacao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Quiron.LojaVirtual.Web.Models
+{
+    public class Paginacao
+    {
+        //Quantos itens tenho no banco
+        public int ItensTotal { get; set; }
+        //Quantos itens quero por pagina
+        public int ItensPorPargina { get; set; }
+        //Qual página esta exibida no momento
+        public int PaginaAtual { get; set; }
+        //Quantas página tem
+        public int TotalPagina
+        {
+            get { return (int) Math.Ceiling((decimal) ItensTotal/ItensPorPargina); }
+        }
+
+
+    }
+}
