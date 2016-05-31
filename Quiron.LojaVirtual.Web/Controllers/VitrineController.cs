adc30047400f9aa0c6dbc746b3b06e1fa63004b7
JaisonSousa <jaison.sp13@gmail.com>
2016-05-31 17:32:54 -0300

    Desenvolvimento

--- Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
+++ Quiron.LojaVirtual.Web/Controllers/VitrineController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Quiron.LojaVirtual.Dominio.Repositorio;
+using Quiron.LojaVirtual.Web.Models;
 
 namespace Quiron.LojaVirtual.Web.Controllers
 {
@@ -17,12 +18,29 @@ namespace Quiron.LojaVirtual.Web.Controllers
         // GET: /Vitrine/
         public ActionResult ListaProdutos(int pagina = 1 )
         {
-            _repositorio = new ProdutosRepositorio();
-            var produtos = _repositorio.Produtos.OrderBy(p => p.Descricao)
-                .Skip((pagina - 1) * ProdutoPorPagina)
-                .Take(ProdutoPorPagina);
 
-            return View(produtos);
+            _repositorio = new ProdutosRepositorio();
+
+            ProdutosViewModel model = new ProdutosViewModel
+            {
+                Produtos = _repositorio.Produtos.OrderBy(p => p.Descricao)
+                    .Skip((pagina - 1)*ProdutoPorPagina)
+                    .Take(ProdutoPorPagina),
+
+                Paginacao = new Paginacao
+                {
+                    PaginaAtual = pagina,
+                    ItensPorPargina = ProdutoPorPagina,
+                    ItensTotal = _repositorio.Produtos.Count()
+                }
+
+
+            };
+
+           
+                
+
+            return View(model);
         }
 	}
 }
