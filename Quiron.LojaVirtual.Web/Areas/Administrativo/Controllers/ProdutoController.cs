f329b3651b1142e671ed8c5ad17b122ef5a94e71
JaisonSousa <jaison.sp13@gmail.com>
2016-08-26 13:02:55 -0300

    Aula 37

--- Quiron.LojaVirtual.Web/Areas/Administrativo/Controllers/ProdutoController.cs
+++ Quiron.LojaVirtual.Web/Areas/Administrativo/Controllers/ProdutoController.cs
@@ -50,19 +50,36 @@ namespace Quiron.LojaVirtual.Web.Areas.Administrativo.Controllers
             return View("Alterar", new Produto());
         }
 
-        public ActionResult Excluir(int produtoId)
+        //public ActionResult Excluir(int produtoId)
+        //{
+        //    _repositorio = new ProdutosRepositorio();
+
+        //    Produto prod = _repositorio.Excluir(produtoId);
+
+        //    if (prod != null)
+        //    {
+        //        TempData["mensagem"] = string.Format("{0} excluído com sucesso", prod.Nome);
+                
+        //    }
+
+        //    return RedirectToAction("Index");
+        //}
+
+        public JsonResult Excluir(int produtoId)
         {
+
+            string mensagem = string.Empty;
             _repositorio = new ProdutosRepositorio();
 
             Produto prod = _repositorio.Excluir(produtoId);
 
             if (prod != null)
             {
-                TempData["mensagem"] = string.Format("{0} excluído com sucesso", prod.Nome);
-                
+                mensagem = string.Format("{0} excluído com sucesso", prod.Nome);
+
             }
 
-            return RedirectToAction("Index");
+            return Json(mensagem, JsonRequestBehavior.AllowGet);
         }
 	}
 }
