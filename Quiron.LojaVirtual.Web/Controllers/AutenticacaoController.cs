5d733d5bbface8f18c96639f6f142303a63beb43
JaisonSousa <jaison.sp13@gmail.com>
2016-09-05 21:33:54 -0300

    aula 40

---
+++ Quiron.LojaVirtual.Web/Controllers/AutenticacaoController.cs
@@ -0,0 +1,69 @@
+﻿using Quiron.LojaVirtual.Dominio.Entidades;
+using Quiron.LojaVirtual.Dominio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class AutenticacaoController : Controller
+    {
+
+        private AdiministradoresRepositorio _repositorio; 
+
+        //Aula 40
+        // GET: /Autenticacao/
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View(new Administrador());
+        }
+
+        public ActionResult Login(Administrador administrador, string returnUrl)
+        {
+            _repositorio = new AdiministradoresRepositorio();
+
+            if (ModelState.IsValid)
+            {
+                Administrador admin = _repositorio.ObterAdiministrador(administrador);
+
+                if (admin != null)
+                {
+                    if (!Equals(administrador.Senha, admin.Senha))
+                    {
+                        ModelState.AddModelError("Senha não confere!");
+
+                       
+                       
+                        
+                    }
+                    else
+                    {
+                        FormsAuthentication.SetAuthCookie(admin.Login,false);
+
+                        if (Url.IsLocalUrl(returnUrl)
+                           && returnUrl > 1
+                           && returnUrl.StartsWith("/")
+                           && !returnUrl.StartsWith("/")
+                           && !returnUrl.StartsWith("/\\"))
+
+                            return Redirect(returnUrl);
+                    }
+                    
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Administrador não localizado");
+                }
+            }
+           
+
+
+        }
+	}
+
+    
+}
