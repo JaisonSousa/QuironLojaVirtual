83bda41a853fd8342921a94adf233c12e2e910c8
JaisonSousa <jaison.sp13@gmail.com>
2016-09-13 18:34:45 -0300

    aula 42

--- Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
+++ Quiron.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -24,5 +24,10 @@ namespace Quiron.LojaVirtual.Dominio.Entidades
 
         [Required(ErrorMessage = "Digite a categoria")]
         public string Categoria { get; set; }
+
+        public byte[] Imagem { get; set; }
+
+        public string ImageMimeType { get; set; }
+
     }
 }
