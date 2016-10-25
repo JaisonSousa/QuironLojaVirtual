0f18a1d8384585f1a83d525bf03c5afe3b52d698
JaisonSousa <jaison.sp13@gmail.com>
2016-10-25 18:12:04 -0300

    Aula 57

--- Quiron.LojaVirtual.Web.V2/App_Start/BundleConfig.cs
+++ Quiron.LojaVirtual.Web.V2/App_Start/BundleConfig.cs
@@ -8,21 +8,32 @@ namespace Quiron.LojaVirtual.Web.V2
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            bundles.Add(new StyleBundle("~/css").Include(
+                     "~/css/*.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/js").Include(
+                     "~/js/jquery.js",
+                      "~/js/bootstrap.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+
+            // <!-- SmartMenu http://www.smartmenus.org/ -->
+            bundles.Add(new StyleBundle("~/Content/startmenu").Include(
+                 "~/Content/sm-core-css.css",
+                // "~/Content/sm-simple/sm-simple.css"
+                   "~/Content/sm-mint/sm-mint.css"
+             ));
+
+            bundles.Add(new ScriptBundle("~/Scripts/startmenu").Include(
+                "~/js/jquery.navgoco.js",
+                "~/Scripts/jquery.smartmenus.js"));
+
+            bundles.Add(new ScriptBundle("~/Scripts/jsprojetos").Include(
+                    "~/Scripts/menu.js"));
+
+            BundleTable.EnableOptimizations = false;
+
+           
         }
     }
 }
